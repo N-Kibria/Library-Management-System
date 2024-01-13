a8d688bd6fe3b5f1d0d3db30db392e64c68eacfa
Nafisa Kibria <nafisa21@iut-dhaka.edu>
2024-01-14 00:39:39 +0600

    Created LibraryEmployee Class

---
+++ src/LibraryEmployee.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class LibraryEmployee  {
+    protected String Name;
+    protected Integer Age;
+    protected Integer ID;
+
+    protected static List<Book> books=new ArrayList<>();
+    protected static List<LibraryMember> members=new ArrayList<>();
+
+
+}
